2979a4073990beb72bb75b4ca23ce3ee6111a7bd
Woradorn K <chateau86@gmail.com>
2018-07-30 13:28:53 +0700

    Signed-off-by: Woradorn K <chateau86@gmail.com>

--- server/routes/api/food.js
+++ server/routes/api/food.js
@@ -2,6 +2,7 @@ const FoodItem = require('../../models/FoodItem');
 
 module.exports = (app) => {
   app.get('/food/list', (req, res, next) => {
+    console.log("get list");
     FoodItem.find()
       .exec()
       .then((food) => res.json(food))
@@ -12,9 +13,10 @@ module.exports = (app) => {
     console.log("add_static_test");
     const food = new FoodItem({
             name: 'green apple', 
-            quantity:3,
+            quantity: 3,
             unit: 'apples',
-            price_per_unit:0.5,
+            price_per_unit:1,
+            date_added:  Date(2018,07,30),
             date_warn:  Date(2018,07,30),
             date_expire:  Date(2018,07,31)
             }); //TODO: Param
