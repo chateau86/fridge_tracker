91d05d134bdb3831e0ae2fc9082d0096c5ca9050
Woradorn K <chateau86@gmail.com>
2018-07-30 14:45:16 +0700

    Added per-food get. Also corrected indents. Signed-off-by: Woradorn K <chateau86@gmail.com>

--- server/routes/api/food.js
+++ server/routes/api/food.js
@@ -1,35 +1,41 @@
 const FoodItem = require('../../models/FoodItem');
 
 module.exports = (app) => {
-  app.get('/food', (req, res, next) => {
-    console.log("get list");
-    FoodItem.find()
-      .exec()
-      .then((food) => res.json(food))
-      .catch((err) => next(err));
-  });
+    app.get('/food', (req, res, next) => {
+        console.log("get list");
+        FoodItem.find()
+            .exec()
+            .then((food) => res.json(food))
+            .catch((err) => next(err));
+    });
+    
+    app.get('/food/:id', (req, res, next) => {
+        FoodItem.findOne({ _id: req.params.id })
+            .exec()
+            .then((food) => res.json(food))
+            .catch((err) => next(err));
+    });
+    app.post('/food/add_static_test', function (req, res, next) {
+        console.log("add_static_test");
+        let food = FoodItem({
+                        name: 'green apple', 
+                        quantity: 3,
+                        unit: 'apples',
+                        price_per_unit:1,
+                        date_warn:    Date(2018,07,30),
+                        date_expire:    Date(2018,07,31)
+                        });
 
-  app.post('/food/add_static_test', function (req, res, next) {
-    console.log("add_static_test");
-    let food = FoodItem({
-            name: 'green apple', 
-            quantity: 3,
-            unit: 'apples',
-            price_per_unit:1,
-            date_warn:  Date(2018,07,30),
-            date_expire:  Date(2018,07,31)
-            });
-
-    food.save()
-      .then(() => res.json(food))
-      .catch((err) => next(err));
-  });
-  
-  app.delete('/food/:id', function (req, res, next) {
-    FoodItem.findOneAndRemove({ _id: req.params.id })
-      .exec()
-      .then((food) => res.json())
-      .catch((err) => next(err));
-  });
+        food.save()
+            .then(() => res.json(food))
+            .catch((err) => next(err));
+    });
+    
+    app.delete('/food/:id', function (req, res, next) {
+        FoodItem.findOneAndRemove({ _id: req.params.id })
+            .exec()
+            .then((food) => res.json())
+            .catch((err) => next(err));
+    });
 
 };
