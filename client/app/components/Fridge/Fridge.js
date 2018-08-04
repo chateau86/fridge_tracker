5336ef5414088513857268cbe2e103f98b2f854a
Woradorn K <chateau86@gmail.com>
2018-08-04 11:59:52 +0700

    Added react-table. Signed-off-by: Woradorn K <chateau86@gmail.com>

--- client/app/components/Fridge/Fridge.js
+++ client/app/components/Fridge/Fridge.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import 'whatwg-fetch';
+import ReactTable from "react-table";
+import 'react-table/react-table.css'
 
 class Fridge extends Component {
   constructor(props) {
