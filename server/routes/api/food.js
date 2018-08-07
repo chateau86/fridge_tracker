78a0901cb4af89a7674a7f35e429cf82891a67fb
Woradorn K <chateau86@gmail.com>
2018-08-07 16:22:20 +0700

    Aggregate test
    Signed-off-by: Woradorn K <chateau86@gmail.com>

--- server/routes/api/food.js
+++ server/routes/api/food.js
@@ -5,10 +5,12 @@ var schedule = require('node-schedule');
  
 var j = schedule.scheduleJob('42 * * * * *', function(){
     console.log('node-schedule test---');
-    FoodItem.find()
-        .exec()
-        .then((food) => console.log(food))
-        .catch((err) => console.log(err));
+    FoodItem.aggregate(
+        { $group: {
+        _id: null,
+        total:       { $sum: { $multiply: ["$price_per_unit", "$quantity"] } }
+        }}
+    )
     console.log('--------');
 });
 
