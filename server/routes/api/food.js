ddedad55b779c4340b9937cc47c6731ca4ad3d76
Woradorn K <chateau86@gmail.com>
2018-08-09 13:29:42 +0700

    Signed-off-by: Woradorn K <chateau86@gmail.com>

--- server/routes/api/food.js
+++ server/routes/api/food.js
@@ -7,7 +7,7 @@ var j = schedule.scheduleJob('42 * * * * *', function(){
     console.log('node-schedule test---');
     console.log(FoodItem.aggregate([
         {$total: {$sum: "$quantity"}}
-        ]).exec())
+        ]).exec().then(res=>console.log(res)))
     console.log('--------');
 });
 
